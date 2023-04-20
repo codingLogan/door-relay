b93f3ed49665edce6f6a727f8786b1f333aca1ea
codingLogan <reachCodingLogan@gmail.com>
2023-04-19 20:50:33 -0600

    Add Door tests with Mock Relay

--- src/doors.py
+++ src/doors.py
@@ -3,24 +3,24 @@ class Doors:
     self.relay = relay
 
   # Returns a list of boolean
-  def get_door_state():
+  def get_door_state(self):
     return self.relay.state()
 
   # Locks unlock when voltage is received
-  def unlock_door(index):
+  def unlock_door(self, index):
     self.relay.turn_on(index)
 
   # Locks lock when voltage is absent
-  def lock_door(index):
+  def lock_door(self, index):
     self.relay.turn_off(index)
 
   # If the door is locked, it will unlock it
   # If the door is unlocked, it will lock it
-  def toggle(index):
+  def toggle(self, index):
     self.relay.toggle(index)
 
-  def unlock_all():
+  def unlock_all(self):
     self.relay.all_on()
 
-  def lock_all():
+  def lock_all(self):
     self.relay.all_off()
--- src/doors.test.py
+++ src/doors.test.py
@@ -22,3 +22,28 @@ relay.all_off()
 assert relay.state() == [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]
 
 print("Finished Mock Tests.")
+print("Start Door Tests...")
+
+door_relay = MockRelay(16)
+doors = Doors(door_relay)
+assert doors.get_door_state() == [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]
+
+doors.unlock_door(2)
+doors.unlock_door(3)
+assert doors.get_door_state() == [False, False, True, True, False, False, False, False, False, False, False, False, False, False, False, False]
+
+doors.lock_door(2)
+assert doors.get_door_state() == [False, False, False, True, False, False, False, False, False, False, False, False, False, False, False, False]
+
+doors.toggle(2)
+assert doors.get_door_state()[2] == True
+doors.toggle(2)
+assert doors.get_door_state()[2] == False
+
+doors.unlock_all()
+assert doors.get_door_state() == [True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]
+
+doors.lock_all()
+assert doors.get_door_state() == [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]
+
+print("Finished Door Tests.")
